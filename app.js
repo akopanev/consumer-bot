4f61ce37b4a707d0ace00dc2211201e82f0f884c
Andrew Kopanev <andrew@moqod.com>
2016-06-14 19:16:37 +0300

    updated keys

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+var bot = new builder.BotConnectorBot({ appId: 'spysebot', appSecret: '773cde6a515144f38949b61cae9db597' });
 bot.add('/', function (session) {
     session.send('Hello World');
 });
