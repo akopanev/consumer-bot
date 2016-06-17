3847a6dafe8d29b762492a95b4964bdfeb07d4fa
Andrew Kopanev <andrew@moqod.com>
2016-06-17 17:01:54 +0300

    updated app.js

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+
+// APIs
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
